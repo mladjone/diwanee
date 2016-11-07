64da696960e0e792dda4a23a38047ffb488ace27
Mladen Orozovic <mladjone@gmail.com>
2016-11-07 22:17:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Cao Skripteri :)");
+$( document ).ready(function(){	
+	$('#openMenu').click(function () {
+        $('nav.mobileNavigation').show(300);
+        $('.fa-bars').hide(300);
+    });
+
+    $('#closeMenu').click(function () {
+        $('nav.mobileNavigation').hide(300);
+        $('.fa-bars').show(300);
+     });
+
+    $('nav.mobileNavigation a').click(function () {
+        $('nav.mobileNavigation').hide(300);
+        $('.fa-bars').show(300);
+    });
+});
